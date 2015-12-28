5db7c10f7b6b632f6a5b1ba34bc85d4641ace5da
hwilson94 <hw1g13@soton.ac.uk>
2015-12-28 21:39:52 +0000

    added new functions into the script and incorporated them into the test, whilst removing code that became duplicated as a consequence

--- The Money Example/testMultiplication.py
+++ The Money Example/testMultiplication.py
@@ -5,11 +5,17 @@ Created on Mon Dec 28 20:27:09 2015
 @author: Harry
 """
 
-def testMultiplication():
-    DollarFive = 5
-    x = 2
-    assert 10 == DollarFive * x
-    y = 3
-    assert 15 == DollarFive * y
 
+def testMultiplication():
+    """no explanation needed"""
+    assert Dollar(10) == FiveTimes(2)
+    assert Dollar(15) == FiveTimes(3)
+
+
+def Dollar(a):
+    return a
+
+
+def FiveTimes(b):
+    return 5 * b
     
