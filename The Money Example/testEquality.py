4fb9a9463b5dfe475a4ab6f1e0606ca6c16711d1
hwilson94 <hw1g13@soton.ac.uk>
2015-12-28 21:28:32 +0000

    created test for equality

---
+++ The Money Example/testEquality.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 21:23:47 2015
+
+@author: Harry
+"""
+
+
+def testEquality():
+    DollarFive = 5
+    assert DollarFive == DollarFive
---
+++ The Money Example/testMultiplication.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 28 20:27:09 2015
+
+@author: Harry
+"""
+
+def testMultiplication():
+    DollarFive = 5
+    x = 2
+    assert 10 == DollarFive * x
+    y = 3
+    assert 15 == DollarFive * y
+
+    
